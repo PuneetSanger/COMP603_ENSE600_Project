b1cc865bfbdfc8d21ff86ab0f2925c5bdcaf0e30
puneetsanger <puneetsanger@localhost>
2024-10-02 23:19:04 +1300

    Initial Commit

---
+++ src/Assignment2/Assignment2.java
@@ -0,0 +1,18 @@
+package Assignment2;
+
+/**
+ *
+ * @author puneetsanger
+ */
+public class Assignment2 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
